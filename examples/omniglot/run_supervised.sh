02fc9b1f5807b67b7eca18e6a5b9c8cdb1a8cd90
Maruan Al-Shedivat <alshedivat.maruan@gmail.com>
2020-04-20 21:05:13 -0400

    Update configs and examples

--- examples/omniglot/run_supervised.sh
+++ examples/omniglot/run_supervised.sh
@@ -2,12 +2,16 @@
 
 NUM_CLASSES=5
 DATASET=omniglot
-DATA_DIR=$(realpath .)/data/$DATASET
+MODEL=feed_forward
+BACKBONE=simple_cnn
+DATA_DIR=$(realpath .)/data/${DATASET}
 
 CUDA_VISIBLE_DEVICES=0 \
 python -u -m meta_blocks.run \
   experiment=${DATASET}/${NUM_CLASSES}way/supervised/oracle \
   evaluation=${DATASET}/${NUM_CLASSES}way \
-  dataset=$DATASET \
+  dataset=${DATASET} \
+  model=${DATASET}/${MODEL} \
+  network=${DATASET}/${BACKBONE} \
   adaptation=maml \
   data.read_config.data_dir=${DATA_DIR}
