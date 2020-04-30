304b8c26c9df278fd11d6e47ea045de0de5e7eae
Maruan Al-Shedivat <alshedivat.maruan@gmail.com>
2020-04-30 17:46:40 -0400

    Add back python 3.6 classifier

--- setup.py
+++ setup.py
@@ -63,6 +63,7 @@ setup(
         "Intended Audience :: Information Technology",
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
     ],
