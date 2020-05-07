0eb052cf55cb1fde54063d6ddf36f8f2ddbe7d46
Maruan Al-Shedivat <alshedivat.maruan@gmail.com>
2020-05-07 08:24:28 -0400

    Update searchpath plugin

--- hydra_plugins/meta_blocks_experiment_searchpath_plugin/meta_blocks_experiment_searchpath_plugin.py
+++ hydra_plugins/meta_blocks_experiment_searchpath_plugin/meta_blocks_experiment_searchpath_plugin.py
@@ -2,6 +2,6 @@ from hydra._internal.config_search_path import ConfigSearchPath
 from hydra.plugins import SearchPathPlugin
 
 
-class MetaBlocksSearchPathPlugin(SearchPathPlugin):
+class MetaBlocksExperimentSearchPathPlugin(SearchPathPlugin):
     def manipulate_search_path(self, search_path: ConfigSearchPath):
-        search_path.append("meta-blocks", "pkg://meta_blocks.conf")
+        search_path.append("meta-blocks", "pkg://meta_blocks.experiment.conf")
